6250775ad3d1bb30f7049b1307102f5254ac837f
Griffin <dooleygriffin@gmail.com>
2023-01-17 13:45:33 -0500

    components closer to finished but still needs css fine tuning

--- pages/components/stack.js
+++ pages/components/stack.js
@@ -1,14 +1,10 @@
-const Stack = ({background, textContent}) => {
+const Stack = ({ background, textContent, accentColor }) => {
   return (
     <div className={`w-100 h-[95vh] ${background} flex items-center`}>
       <div className="text-xl w-2/5 p-36 rounded-lg font-semibold text-black ml-[3rem]">
         <span>Lorem ipsum+. </span>
-        <span className="opacity-40">
-          Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </span>
-        <div className="text-blue-600 mt-4 hover:underline decoration-2 underline-offset-2 cursor-pointer">
+        <span className="opacity-40">{textContent}</span>
+        <div className={`text-blue-600 ${accentColor} mt-4 hover:underline decoration-2 underline-offset-2 cursor-pointer`}>
           Learn More
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 import Stack from "./components/stack";
 
 const Home: NextPage = () => {
-
   return (
     <div>
       <Head>
@@ -15,30 +14,75 @@ const Home: NextPage = () => {
 
       <div className="">
         <div className="w-[55rem] h-[29.3rem] fixed z-10 right-[-100px] top-48 shadow-xl border-l-4 border-y-4 border-black overflow-hidden">
-          <motion.video className="w-[110%] sticky" autoPlay loop muted src={"/surfer.mp4"} />
-          <motion.video className="w-[110%]" autoPlay loop muted src={"/volcano.mp4"} />
-          <motion.video className="w-[110%]" autoPlay loop muted src={"/sailboat.mp4"} />
-          <motion.video className="w-[110%]" autoPlay loop muted src={"/massage.mp4"} />
+          <motion.video
+            className="w-[110%] sticky"
+            autoPlay
+            loop
+            muted
+            src={"/surfer.mp4"}
+          />
+          <motion.video
+            className="w-[110%]"
+            autoPlay
+            loop
+            muted
+            src={"/volcano.mp4"}
+          />
+          <motion.video
+            className="w-[110%]"
+            autoPlay
+            loop
+            muted
+            src={"/sailboat.mp4"}
+          />
+          <motion.video
+            className="w-[110%]"
+            autoPlay
+            loop
+            muted
+            src={"/massage.mp4"}
+          />
         </div>
       </div>
 
       <main>
-        <div className="w-100 h-[95vh] bg-white flex items-center" id="white">
-          <div className="text-xl w-2/5 p-36 rounded-lg font-semibold text-black ml-[3rem]">
-            <span>Lorem ipsum+. </span>
-            <span className="opacity-40">
-              Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        <Stack
+          background={"bg-white"}
+          textContent="Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-            </span>
-            <div className="text-blue-600 mt-4 hover:underline decoration-2 underline-offset-2 cursor-pointer">
-              Learn More
-            </div>
-          </div>
-        </div>
+              consequat."
+          accentColor=""
+        />
 
-        <div className="w-100 h-[90vh] bg-green-500 flex items-center" id="green">
+        <Stack
+          background={"bg-green-500"}
+          textContent="dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis laboris nisi ut aliquip ex ea commodo."
+          accentColor=""
+        />
+
+        <Stack
+          background={"bg-violet-700"}
+          textContent="dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              exercitation ullamco nisi ut aliquip ex ea commodo laboris."
+          accentColor=""
+        />
+
+        <Stack
+          background={"bg-red-500"}
+          textContent="dolor sit amet, consectetur elit, sed do tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris ex ea commodo adipiscing."
+          accentColor=""
+        />
+
+        <div
+          className="w-100 h-[90vh] bg-green-500 flex items-center"
+          id="green"
+        >
           <div className="text-xl w-2/5 p-36 rounded-lg font-semibold text-white ml-[3rem]">
             <span>Lorem ipsum </span>
             <span className="opacity-60">
@@ -51,34 +95,6 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-
-        <div className="w-100 h-[90vh] bg-violet-700 flex items-center" id="violet">
-          <div className="text-xl w-2/5 p-36 rounded-lg font-semibold text-white ml-[3rem]">
-            <span>Lorem ipsum </span>
-            <span className="opacity-60">
-              dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt
-              ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              exercitation ullamco nisi ut aliquip ex ea commodo laboris.
-            </span>
-            <div className="text-white mt-4 hover:underline decoration-2 underline-offset-2 cursor-pointer">
-              Learn More
-            </div>
-          </div>
-        </div>
-        <div className="w-100 h-[95vh] bg-red-500 flex items-center" id="red">
-          <div className="text-xl w-2/5 p-36 rounded-lg font-semibold text-white ml-[3rem]">
-            <span>Lorem ipsum </span>
-            <span className="opacity-60">
-              dolor sit amet, consectetur elit, sed do tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris ex ea commodo adipiscing.
-            </span>
-            <div className="text-white mt-4 hover:underline decoration-2 underline-offset-2 cursor-pointer">
-              Learn More
-            </div>
-          </div>
-        </div>
-        <Stack background={"bg-blue-400"} textContent={""}/>
       </main>
     </div>
   );
